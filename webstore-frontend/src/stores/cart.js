a88c034bbead5e2cf7c1849f56c3132004fd33de
Mustafa Ansari <mustafa.ansari1908@outlook.com>
2024-10-25 07:57:52 +0500

    localstorage Cart

--- webstore-frontend/src/stores/cart.js
+++ webstore-frontend/src/stores/cart.js
@@ -1,43 +1,55 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { ref, computed, watch } from 'vue'
 
 export const useCartStore = defineStore('cart', () => {
-  // State: Cart items
-  const cartItems = ref([])
+  // Load initial cart items from localStorage
+  const cartItems = ref(JSON.parse(localStorage.getItem('cartItems')) || [])
+
+  // Function to update localStorage
+  const updateLocalStorage = () => {
+    localStorage.setItem('cartItems', JSON.stringify(cartItems.value))
+  }
+
+  // Save cart items to localStorage whenever they change
+  watch(cartItems, updateLocalStorage)
 
   // Add an item to the cart
   const addItemToCart = (item) => {
     const existingItem = cartItems.value.find(cartItem => cartItem.id === item.id)
     if (existingItem) {
-        if (existingItem.stock > existingItem.quantity)
-            existingItem.quantity++
+      if (existingItem.stock > existingItem.quantity) {
+        existingItem.quantity++
+      }
     } else {
       cartItems.value.push({ ...item, quantity: 1 })
     }
+    updateLocalStorage() // Update localStorage here
   }
 
   // Increment the quantity of an item
   const incrementQuantity = (itemID) => {
-    const item = cartItems.value.find(cartItem => cartItem.id === itemID);
+    const item = cartItems.value.find(cartItem => cartItem.id === itemID)
     if (item) {
-      item.quantity++;
+      item.quantity++
+      updateLocalStorage() // Update localStorage here
     }
-  };
-  
+  }
+
   // Decrement the quantity of an item
   const decrementQuantity = (itemID) => {
-    const item = cartItems.value.find(cartItem => cartItem.id === itemID);
+    const item = cartItems.value.find(cartItem => cartItem.id === itemID)
     if (item && item.quantity > 1) {
-      item.quantity--;
+      item.quantity--
     } else {
       // Optionally, remove the item when quantity is zero
-      cartItems.value = cartItems.value.filter(cartItem => cartItem.id !== itemID);
+      cartItems.value = cartItems.value.filter(cartItem => cartItem.id !== itemID)
     }
-  };
+    updateLocalStorage() // Update localStorage here
+  }
 
   const totalItems = computed(() => {
-    return cartItems.value.reduce((total, item) => total + item.quantity, 0);
-  });
+    return cartItems.value.reduce((total, item) => total + item.quantity, 0)
+  })
 
   return {
     cartItems,
